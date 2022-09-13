3b037289bab719306faac278ee183132948e73e1
Maik Schöpe <m.schoepe@ccgmbh.de>
2022-09-13 20:27:06 +0200

    sqlite testing added

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,27 +9,29 @@ from wire_service.settings import settings
 
 @pytest.fixture(scope="session", autouse=True)
 def set_test_settings():
-    settings.configure(FORCE_ENV_FOR_DYNACONF="testing")
+    settings.configure(FORCE_ENV_FOR_DYNACONF="sqlite_testing")
+    # settings.configure(FORCE_ENV_FOR_DYNACONF="testing")
 
 
 @pytest.fixture(scope="session", autouse=True)
 def ensure_db_exists(set_test_settings):
-    con = psycopg2.connect(
-        f"dbname='postgres' user='{settings.DB_USER}' host='{settings.DB_HOST}' port={settings.DB_PORT} password='{settings.DB_PASS}'"
-    )
-    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    if settings.FORCE_ENV_FOR_DYNACONF != "sqlite_testing":
+        con = psycopg2.connect(
+            f"dbname='postgres' user='{settings.DB_USER}' host='{settings.DB_HOST}' port={settings.DB_PORT} password='{settings.DB_PASS}'"
+        )
+        con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-    cursor = con.cursor()
+        cursor = con.cursor()
 
-    cursor.execute(
-        f"select exists(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('{settings.DB_NAME}'));"
-    )
-    exists = cursor.fetchone()[0]
+        cursor.execute(
+            f"select exists(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('{settings.DB_NAME}'));"
+        )
+        exists = cursor.fetchone()[0]
 
-    if not exists:
-        cursor.execute(f"CREATE DATABASE {settings.DB_NAME};")
+        if not exists:
+            cursor.execute(f"CREATE DATABASE {settings.DB_NAME};")
 
-    con.close()
+        con.close()
 
     Base.metadata.drop_all(DbConnection.engine)
     Base.metadata.create_all(DbConnection.engine)
